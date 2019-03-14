b673f605ac142a4c77f451573bd56559bdafae62
victortruan <victor.truan@heig-vd.ch>
2019-03-14 11:16:52 +0100

    Utils pass test

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
@@ -1,7 +1,7 @@
 package ch.heigvd.res.labio.impl;
 
 import java.util.logging.Logger;
-
+import java.util.*;
 /**
  *
  * @author Olivier Liechti
@@ -20,7 +20,22 @@ public class Utils {
    * contain any line separator, then the first element is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    //If the string contains only /r but no /n we split on it and add it to the first string.
+    if(lines.contains("\r") && !lines.contains("\n")){
+      String[] tmp = lines.split("\r",2);
+      tmp[0] += "\r";
+      return tmp;
+      //If the string contains /n, we split on it and add it to the first String
+      //If the char before is /r or another, the result is the same.
+    }else if(lines.contains("\n")){
+      String[] tmp = lines.split("\n",2);
+      tmp[0] += "\n";
+      return tmp;
+      //If there is no line delimiter the first element is an empty string and the second the full string.
+    }else{
+      return new String[]{"",lines};
+    }
+
   }
 
 }
